26399a40e4982a4c31046a8d12247918c070aca4
dariush.fathe <dariush.fathe@gmail.com>
2020-02-15 21:23:54 +0330

    new commit

--- App.js
+++ App.js
@@ -26,52 +26,31 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+import {Card} from './src/models/jinPost';
+
+export default class App extends Component{
+  Card = (rank, suit) => {
+    this.rank = rank;
+    this.suit = suit;
+  };
+
+  createCard = () => {
+    let CardObj = {};
+    let card = new this.Card('3', 'Hearts');
+    console.log(card);
+  };
+
+  render(){
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+       <View >
+         <Text>behzad</Text>
+       </View>
+      </>
+    );
+  }
+  
 };
 
 const styles = StyleSheet.create({
@@ -113,4 +92,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+
