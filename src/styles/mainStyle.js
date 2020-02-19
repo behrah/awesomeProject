72d94e739d5432bd1bffa6adf60720fa34b00ed3
behrah <behrah136641@gmail.com>
2020-02-19 14:00:14 +0330

    add styles folder

---
+++ src/styles/mainStyle.js
@@ -0,0 +1,10 @@
+
+import {StyleSheet} from 'react-native'
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:'#f3f6f8'
+    }
+   });
+
+   export default styles;
