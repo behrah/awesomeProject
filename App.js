2cbc1af603b515194a25dc65a901e9a5eab8a129
behrah <behrah136641@gmail.com>
2020-02-20 13:50:14 +0330

    new commit

--- App.js
+++ App.js
@@ -16,22 +16,34 @@ import {
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import styles from './src/styles/mainStyle'
+import styles from './src/styles/mainStyle';
+import ModalExample from './src/views/jinPostView';
 
 const App: () => React$Node = () => {
-  return (
-    <View style={styles.container}></View>
-  );
+
+
+  getData() = {
+    fetch('https://req.jin724.com/beta/v2/getStructureList/JIN-3382-2df1bc-8bfeea-4123c7-9f096d-b43b7e', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        post: '0',
+        structureId: '291220',
+        ownerId: '1',
+        more: '0',
+        filter: '',
+      }),
+  });
+}
+
+return (
+  <View>
+    <ModalExample />
+  </View>
+);
 };
-
-
 
 export default App;
