0e38be3a1042cb98b5356f49a0a4cd65551be836
behrah <behrah136641@gmail.com>
2020-02-29 13:03:11 +0330

    new commit

--- App.js
+++ App.js
@@ -6,27 +6,46 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
+  Button,
+  TextInput,
 } from 'react-native';
 
 import styles from './src/styles/mainStyle';
 
-const App: () => React$Node = () => {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
 
-return (
-  <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-    <View style={{width:50,height:50,backgroundColor:'red'}}></View>
-    <View style={{width:50,height:50,backgroundColor:'green'}}></View>
-    <View style={{width:50,height:50,backgroundColor:'blue'}}></View>
-  </View>
-);
-};
-
-export default App;
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text style={{backgroundColor: 'red'}}>
+          salam {this.state.text} aziz
+        </Text>
+        <TextInput
+          style={{flex: 1, height: 40}}
+          placeholder="enter your name"
+          value={this.state.text}
+          onTextChange={(text)=>this.setState({text})}
+        />
+        
+      </View>
+    );
+  }
+}
