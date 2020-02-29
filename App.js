1613ec42fd63d0a7ea9ed02a663370d380bec81f
behrah <behrah136641@gmail.com>
2020-02-29 11:55:34 +0330

    new commit

--- App.js
+++ App.js
@@ -17,31 +17,14 @@ import {
 } from 'react-native';
 
 import styles from './src/styles/mainStyle';
-import ModalExample from './src/views/jinPostView';
 
 const App: () => React$Node = () => {
 
-
-  getData() = {
-    fetch('https://req.jin724.com/beta/v2/getStructureList/JIN-3382-2df1bc-8bfeea-4123c7-9f096d-b43b7e', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        post: '0',
-        structureId: '291220',
-        ownerId: '1',
-        more: '0',
-        filter: '',
-      }),
-  });
-}
-
 return (
-  <View>
-    <ModalExample />
+  <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+    <View style={{width:50,height:50,backgroundColor:'red'}}></View>
+    <View style={{width:50,height:50,backgroundColor:'green'}}></View>
+    <View style={{width:50,height:50,backgroundColor:'blue'}}></View>
   </View>
 );
 };
